9ed3a42cf1bad6f20bb2a7cb674ac2ea4588e133
tripleKS <skorenko@gmail.com>
2015-01-27 14:24:59 +0300

    Adding example of db dumping and afterwards sql script running

--- examples/downloadMySQLDump.groovy
+++ examples/downloadMySQLDump.groovy
@@ -1,6 +1,8 @@
 /*
  * This script connects to remote server on which MySQL server is running.
- * It creates dump of MySQL db and copies it locally.
+ * It executes sql script on the remote MySQL server.
+ * Before running sql script the current script creates dump of the remote db and copies it locally.
+ * After copying the db dump it executes sql script via sql.exe. In case of error the current script copies MySQL script to the local host.
  */
 
 @Grab('com.aestasit.infrastructure.groowin:groowin:0.1.11-SNAPSHOT')
@@ -10,23 +12,35 @@ import static com.aestasit.infrastructure.winrm.DefaultWinRM.*
 def user = 'vagrant'
 def pwd = 'vagrant'
 def host = '192.168.25.25'
-def port = '5986'
+def port = '5985'
 
 def mySqlUser = 'root'
 def mySqlPassword = 'secret'
 
 def currentTime = new Date().format('yyyy-MM-dd HH-mm')
-def folderToSaveDump = "applicationDBDump/sakila ${currentTime}/"
+def folderToSaveDump = "applicationDBDump/application ${currentTime}/"
 def dumpStorage = new File(folderToSaveDump)
 if (!dumpStorage.exists()) {
   dumpStorage.mkdirs()
 }
 
 remoteManagement("${user}:${pwd}@${host}:${port}") {
-  exec "c:\\mysql\\bin\\mysqldump --user=${mySqlUser} --password=${mySqlPassword} sakila > C:\\sakila_dmp.sql"
+  exec "c:\\mysql\\bin\\mysqldump --user=${mySqlUser} --password=${mySqlPassword} application_db > c:\\temp\\appScripts\\appl_dmp.sql"
   cp {
-    from {remoteFile 'C:\\sakila_dmp.sql'}
+    from {remoteFile 'c:\\temp\\appScripts\\appl_dmp.sql'}
     into {localDir folderToSaveDump}
+  }
+
+  cp{
+    from {localFile 'change_db_schema.sql'}
+    into {remoteFile 'c:\\temp\\appScripts\\change_db_schema.sql'}
+  }
+  def result = exec "c:\\mysql\\bin\\mysql --user=${mySqlUser} --password=${mySqlPassword} application_db < c:\\temp\\appScripts\\change_db_schema.sql"
+  if(result.exitStatus){
+    cp{
+      from {remoteDir 'c:\\temp\\mySqlLogs\\'}
+      into {localDir folderToSaveDump}
+    }
   }
 }
 
